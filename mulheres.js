ba6f1342757a84ab5382956c657d4e9a42e131fe
VeronicaCMuller <veronicacamposmuller@gmail.com>
2023-05-08 21:15:47 -0300

    Insere a nova versão do projeto, integrado com o MongoDB

--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,99 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") // aqui estou iniciando o express
+const router = express.Router() // aqui estou configurando a primeira parte da rota
+const cors = require ('cors') // aqui estou trazendo o pacote cors, que permite consumir essa api no front-end 
 
-const app = express()
-const porta = 3333;
+const conectaBancoDeDados = require('./bancoDeDados') // aqui estou ligando ao arquivo bancoDeDados
+conectaBancoDeDados() // estou chamando a função que conectaBancoDeDados
 
-const mulheres = [
-    {
-        nome: "Giulia Bordignon",
-        image: "https://media.licdn.com/dms/image/D4D03AQGGhkw_6vsqww/profile-displayphoto-shrink_800_800/0/1675271125950?e=1687996800&v=beta&t=EAxnyoltZY0rfQAVjWbz6Fsvle3ytfLR1VEkhLCAWNc",
-        minibio: "Desenvolvedora Back-end, e criadora de conteúdo digital"
-    },
-    {
-        nome: "Simara Conceição",
-        image: "https://media.licdn.com/dms/image/C4E03AQFAcqqo2WX_8A/profile-displayphoto-shrink_800_800/0/1563116714583?e=1687996800&v=beta&t=aTIGSJRaoe538gP-CvOaaAdwXqHOwGIyHo9w6r9hAtU",
-        minibio: "Desenvolvedora FullStack e instrutora"
-    },
-    {
-        nome: "Iana Chan",
-        image: "https://media.licdn.com/dms/image/C4D03AQE-aD7nj2W_0Q/profile-displayphoto-shrink_800_800/0/1563383651406?e=1687996800&v=beta&t=f2NCre8pRfROBebkmW8CzzRKqwAHXygFHqMVwAk5bfs",
-        minibio: "Fundadora da PrograMaria"
+const Mulher = require('./mulherModel')
+
+const app = express() // aqui estou iniciando o app
+app.use(express.json())
+app.use(cors())
+const porta = 3333;  // aqui estou criando a porta
+
+
+
+
+//GET
+async function mostrarMulheres (request ,response){
+    try{
+    const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+    response.json (mulheresVindasDoBancoDeDados)
+
+    }catch(erro){
+        console.log(erro)
     }
-]
-
-function mostrarMulheres (request ,response){
-    response.json(mulheres)
 }
 
+//POST
+async function criaMulher(request, response){
+    const novaMulher = new Mulher({
+        nome: request.body.nome,
+        image: request.body.image,
+        minibio: request.body.minibio,
+        citacao: request.body.citacao
+    })
+    try{
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    }catch (erro){
+        console.log(erro)
+    }
+
+
+}
+
+//PATCH
+async function corrigeMulher(request, response){
+  try{
+    const mulherEncontrada = await Mulher.findById(request.params.id)
+
+    if (request.body.name) {
+        mulherEncontrada.nome = request.body.nome
+    }
+
+    if (request.body.image){
+        mulherEncontrada.image = request.body.image
+    }
+
+    if (request.body.minibio){
+        mulherEncontrada.minibio = request.body.minibio
+    }
+    if (request.body.citacao){
+        mulherEncontrada.citacao = request.body.citacao
+    }
+
+    const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+    
+    response.json(mulherAtualizadaNoBancoDeDados)
+  }catch (erro){
+    console.log(erro)
+  }
+}
+
+// DELETE
+async function deletaMulher(request,response){
+
+    try{
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({mensagem: 'Mulher deletada com sucesso.'})
+    }catch(erro){
+        console.log(erro)
+    }
+    
+}
+
+app.use(router.get("/mulheres", mostrarMulheres)) // segunda configuração da rota, rota GET / mulheres
+app.use(router.post("/mulheres", criaMulher))// configurando rota POST/mulheres
+app.use(router.patch("/mulheres/:id", corrigeMulher)) //configurei a rota PATCH/mulheres/:id
+app.use(router.delete("/mulheres/:id" , deletaMulher)) // configurei aqui a nossa rota DELETE/mulheres/:id
+
+//Porta
 function mostrarPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get("/mulheres", mostrarMulheres))
-app.listen(porta, mostrarPorta)
+
+app.listen(porta, mostrarPorta) // servidor ouvindo a porta
